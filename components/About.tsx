8f4435253613c3425431413b039c338bb8d97ab8
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-09-12 17:24:55 +0300

    feat: add relevant sections

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+export default function About() {
+  return (
+    <section className="px-24 flex justify-between" id="home">
+        <div id="words_left" className="w-[50%] pt-24">
+            <h1 className="text-6xl font-bold leading-normal"><span className="underline underline-offset-4 decoration-[#F57906]">Lets</span> Create A Memorable Journey</h1>
+            <div className="pr-10">
+                <p className="text-sm leading-loose pt-3">Eestie Explorer helps you discover magical places in Estonia and allows you to customize the places you would like to visit.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
